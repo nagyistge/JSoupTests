8b930efe42584aaba101da5bac304cba8a253516
continuumsecurity <stephen@continuumsecurity.net>
2014-07-06 11:20:11 +0200

    another attack

--- src/test/java/net/continuumsecurity/JSoupXMLDoSTest.java
+++ src/test/java/net/continuumsecurity/JSoupXMLDoSTest.java
@@ -4,6 +4,7 @@ import org.jsoup.Jsoup;
 import org.jsoup.safety.Whitelist;
 import org.junit.Test;
 
+import java.util.ArrayList;
 import java.util.Date;
 
 /**
@@ -14,15 +15,34 @@ public class JSoupXMLDoSTest {
 
     @Test
     public void testExpandingEntities() {
-        String attack = "<!DOCTYPE evildoc [\n" +
+        ArrayList<String> attacks = new ArrayList<>();
+        attacks.add("<!DOCTYPE evildoc [\n" +
                 "<!ENTITY x0 hello XDoS\">\n" +
                 "<!ENTITY xevilparam &x99;&x99;\"> ]>\n" +
-                "<foobar>&xevilparam;</foobar>";
+                "<foobar>&xevilparam;</foobar>");
 
-        Date now = new Date();
-        String output = Jsoup.clean(attack, Whitelist.basic());
-        System.out.println(output);
-        Date after = new Date();
-        assert (after.getTime() - now.getTime()) < 1000;
+        attacks.add("<?xml version=\"1.0\"?>\n" +
+                "<!DOCTYPE lolz [\n" +
+                "  <!ENTITY lol \"lol\">\n" +
+                "  <!ENTITY lol2 \"&lol;&lol;&lol;&lol;&lol;&lol;&lol;&lol;&lol;&lol;\">\n" +
+                "  <!ENTITY lol3 \"&lol2;&lol2;&lol2;&lol2;&lol2;&lol2;&lol2;&lol2;&lol2;&lol2;\">\n" +
+                "  <!ENTITY lol4 \"&lol3;&lol3;&lol3;&lol3;&lol3;&lol3;&lol3;&lol3;&lol3;&lol3;\">\n" +
+                "  <!ENTITY lol5 \"&lol4;&lol4;&lol4;&lol4;&lol4;&lol4;&lol4;&lol4;&lol4;&lol4;\">\n" +
+                "  <!ENTITY lol6 \"&lol5;&lol5;&lol5;&lol5;&lol5;&lol5;&lol5;&lol5;&lol5;&lol5;\">\n" +
+                "  <!ENTITY lol7 \"&lol6;&lol6;&lol6;&lol6;&lol6;&lol6;&lol6;&lol6;&lol6;&lol6;\">\n" +
+                "  <!ENTITY lol8 \"&lol7;&lol7;&lol7;&lol7;&lol7;&lol7;&lol7;&lol7;&lol7;&lol7;\">\n" +
+                "  <!ENTITY lol9 \"&lol8;&lol8;&lol8;&lol8;&lol8;&lol8;&lol8;&lol8;&lol8;&lol8;\">\n" +
+                "]>\n" +
+                "<lolz>&lol9;</lolz>");
+
+        for (String attack : attacks) {
+            Date now = new Date();
+            String output = Jsoup.clean(attack, Whitelist.basic());
+            System.out.println(output);
+            Date after = new Date();
+            assert (after.getTime() - now.getTime()) < 1000;
+        }
     }
+
+
 }
